480ed6f25ebf426eb65b1ed9a0e0a68cedde65a6
freecontent <84922944+freecontent@users.noreply.github.com>
2021-11-22 13:36:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,27 +3,53 @@ require('dotenv').config();
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 const axios = require('axios');
-
 const path = require('path');
-// const router = express.Router();
+
+const { Client } = require('pg');
+const client = new Client({
+    host: 'ec2-18-208-97-23.compute-1.amazonaws.com',
+    user: 'pamtcqktcecmyb',
+    password: '51ca24c01af487cc33e99e5488bacc82d1d176ae7e55d1f0e2f43c61041f66dd',
+    database: 'd51cks1n66f7gd',
+    port: 5432,
+    ssl: { rejectUnauthorized: false }
+});
+
+client.connect();
 
 const express = require('express');
 const app = express();
-
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use('/', router);
+app.get('*', async (req, res) => {
+    console.log('req===', req.path);
+    let cryptID = '';
 
-app.get('*', (req, res) => {
-    // res.send('Bot is working now !!');
-    console.log('req===', req.path)
-    console.log('req.route.path', req.path.split('/')[1])
-    res.render(path.join(__dirname+'/index.ejs'), {
-        username: 'sagar'
-    })
-    // res.sendFile(path.join(__dirname+'/index.html'));
-    // res.sendFile('index.html');
+    if (req.path) cryptID = req.path.split('/')[1];
+
+    console.log('cryptID==', cryptID)
+
+    if (cryptID) {
+        await client.query(`SELECT droplink FROM tg_droplink_data WHERE uniq_id = \'${cryptID}\'`, (err, result) => {
+            if (err) {
+                console.log('errr', err);
+                return;
+            }
+            else {
+                console.log('results', result);
+                if (result.rows.length) {
+                    res.render(path.join(__dirname + '/index.ejs'), {
+                        cryptID: cryptID
+                    });
+                } else {
+                    res.send('File is removed or something went wrong with the file. Please contact owner !!');
+                }
+            }
+        });
+    } else {
+        res.send('Welcome !!');
+    }
 });
 
 app.listen(process.env.PORT || 5000);
@@ -61,26 +87,63 @@ bot.start((ctx) => {
     });
 });
 
+bot.command('check', (ctx) => {
+    client.query(`SELECT * FROM tg_droplink_data`, (err, result) => {
+        if (err) {
+            ctx.reply('Something went wrong !!');
+            console.log('errr', err);
+        }
+        else {
+            console.log('results', result);
+        }
+    });
+});
+
 bot.on('text', async (ctx) => {
     console.log('ctx', ctx.message.text);
+    const URL = ctx.message.text;
 
     const urlRegex = /(https?:\/\/[^\s]+)/g;
-    const URL = ctx.message.text.match(urlRegex);
-    console.log('url---', URL)
+    const shortURL = ctx.message.text.match(urlRegex);
+    console.log('url---', shortURL)
 
-    if (URL.length) {
-        const response = {
-            data: {
-                status: 'success',
-                message: '',
-                shortenedUrl: 'https://droplink.co/QHVc'
+    if (shortURL.length) {
+        // check in db exists or not
+        let isExists = false;
+
+        const findQuery = `SELECT droplink FROM tg_droplink_data WHERE org_url = \'${URL}\'`
+
+        client.query(findQuery, async (err, result) => {
+            if (err) {
+                ctx.reply('Something went wrong !!');
+                console.log('errr', err);
+                return;
             }
-        }
-        // const response = await axios.get(`https://droplink.co/api?api=${process.env.DROPLINK_API_TOKEN}&url=${URL[0]}`);
-        console.log('response', response)
-        if (response.data.status === 'success') {
-            ctx.reply(response.data.shortenedUrl);
-        }
+            else {
+                console.log('results', result);
+                if (result.rows.length) isExists = true;
+
+                if (isExists) return ctx.reply('Already short link added ==> link');
+
+                const uniqID = (new Date()).getTime().toString(36);
+                const linkToShort = `https://droplink-bot.herokuapp.com/${uniqID}`
+                const response = await axios.get(`https://droplink.co/api?api=${process.env.DROPLINK_API_TOKEN}&url=${linkToShort}`);
+
+                console.log('response', response)
+                if (response.data.status === 'success') {
+                    const insertQuery = `INSERT INTO tg_droplink_data (droplink, org_url, uniq_id) VALUES (\'${response.data.shortenedUrl}\', \'${URL}\', \'${uniqID}\')`
+
+                    client.query(insertQuery, (err, result) => {
+                        if (err) {
+                            ctx.reply('Something went wrong !!');
+                            return console.log('errr', err);
+                        }
+                        ctx.reply(response.data.shortenedUrl);
+                        console.log('results', result);
+                    });
+                }
+            }
+        });
     }
 });
 
