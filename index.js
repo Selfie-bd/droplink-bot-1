54cae2e526bf3171a255228b62598af7daaedeae
freecontent <84922944+freecontent@users.noreply.github.com>
2021-11-23 14:18:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,19 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 const axios = require('axios');
 const path = require('path');
 
+const ffmpeg = require('fluent-ffmpeg');
+const fs = require('fs');
+
+const db = require('./queries');
+
 const { Client } = require('pg');
 const client = new Client({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
+    host: process.env.DBHOST,
+    user: process.env.DBUSER,
+    password: process.env.DBPASSWORD,
     database: process.env.DATABASE,
-    port: process.env.DBPORT
+    port: process.env.DBPORT,
+    ssl: { rejectUnauthorized: false }
 });
 
 client.connect();
@@ -21,33 +27,23 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get('*', async (req, res) => {
-    console.log('req===', req.path);
-    let cryptID = '';
+app.get('/', async (req, res) => {
+    res.send('Welcome !!');
+});
 
-    if (req.path) cryptID = req.path.split('/')[1];
+app.get('/:id', async (req, res) => {
+    if (req.params.id === 'favicon.ico') return;
+    console.log('req===', req);
+    const results = await db.getDataByUniqId(req, res);
+    console.log('hello', results);
 
-    console.log('cryptID==', cryptID)
-
-    if (cryptID) {
-        await client.query(`SELECT org_url FROM tg_droplink_data WHERE uniq_id = \'${cryptID}\'`, (err, result) => {
-            if (err) {
-                console.log('errr', err);
-                return;
-            }
-            else {
-                console.log('results', result);
-                if (result.rows.length) {
-                    res.render(path.join(__dirname + '/index.ejs'), {
-                        url: `intent://${result.rows.org_url}#Intent;package=com.playit.videoplayer;action=android.intent.action.VIEW;scheme=http;type=video/mp4;end`
-                    });
-                } else {
-                    res.send('File is removed or something went wrong with the file. Please contact owner !!');
-                }
-            }
+    if (results.length) {
+        res.render(path.join(__dirname + '/index.ejs'), {
+            video: results[0].org_url,
+            url: `intent://${results[0].org_url}#Intent;package=com.playit.videoplayer;action=android.intent.action.VIEW;scheme=http;type=video/mp4;end`
         });
     } else {
-        res.send('Welcome !!');
+        res.send('File is removed or something went wrong with the file. Please contact owner !!');
     }
 });
 
@@ -88,6 +84,30 @@ bot.start((ctx) => {
 
 bot.command('check', (ctx) => {
     client.query(`SELECT * FROM tg_droplink_data`, (err, result) => {
+        if (err) {
+            ctx.reply('Something went wrong !!');
+            console.log('errr', err);
+        }
+        else {
+            console.log('results', result);
+        }
+    });
+});
+
+// bot.command('delete', (ctx) => {
+//     client.query(`DELETE FROM tg_droplink_data WHERE id = 1`, (err, result) => {
+//         if (err) {
+//             ctx.reply('Something went wrong !!');
+//             console.log('errr', err);
+//         }
+//         else {
+//             console.log('results', result);
+//         }
+//     });
+// });
+
+bot.command('delete_all', (ctx) => {
+    client.query(`DELETE FROM tg_droplink_data WHERE id = 1`, (err, result) => {
         if (err) {
             ctx.reply('Something went wrong !!');
             console.log('errr', err);
@@ -144,6 +164,60 @@ bot.on('text', async (ctx) => {
             }
         });
     }
+});
+
+async function downloadImage(url, path) {
+    const writer = fs.createWriteStream(path)
+
+    const response = await axios({
+        url,
+        method: 'GET',
+        responseType: 'stream'
+    })
+
+    response.data.pipe(writer)
+
+    return new Promise((resolve, reject) => {
+        writer.on('finish', resolve)
+        writer.on('error', reject)
+    })
+};
+
+bot.command('test', async (ctx) => {
+    console.log('test-command-568');
+    if(!fs.existsSync('uploads')) {
+        fs.mkdirSync('uploads');
+    };
+    console.log('572');
+    const localFilePath = "./uploads/Hello.mp4"
+    console.log('file-is-going-to-be-saved', localFilePath);
+    await downloadImage('https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4', localFilePath);
+    console.log('file-is-saved');
+    
+    if(!fs.existsSync('./uploads/Hello.mp4')) console.log('not-existed');
+    
+    try {
+        console.log('gone-in-try');
+        ffmpeg('./uploads/Hello.mp4')
+        .on('filenames', function(filenames) {
+            console.log('filenames', filenames);
+            ctx.replyWithPhoto(path.join(__dirname + `/downloads/${filenames}`));
+         })
+        .on('end', function() {
+            console.log('Screenshots taken');
+         })
+        .on('error', function(err) {
+            console.error(err);
+         })
+        .screenshots({
+            // Will take screenshots at 20%, 40%, 60%, 80% and 100% of the video
+            count: 5,
+            folder: './downloads/'
+        });
+    }
+    catch (error){
+        console.log('try-catch-error',error)
+    }  
 });
 
 bot.launch();
