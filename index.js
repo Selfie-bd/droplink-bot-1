d685e3138a6700e06debe66c14eb0f37bde530d6
freecontent <84922944+freecontent@users.noreply.github.com>
2021-11-25 17:57:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.get('/:id', async (req, res) => {
     console.log('res--', results);
     if (results.total > 0) {
         const intentUrl = results.data[0].org_url.replace(/(^\w+:|^)\/\//, '');
-        res.render(path.join(__dirname + './index.ejs'), {
+        res.render(path.join(__dirname + '/public/ejs/index.ejs'), {
             video: results.data[0].org_url,
             video_name: results.data[0].video_name,
             video_size: results.data[0].video_size,
